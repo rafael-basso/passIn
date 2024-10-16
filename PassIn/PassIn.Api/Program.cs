f22e267a4da20cca42e8117ec496760f2424bd12
rafael-basso <rafael_basso81@hotmail.com>
2024-10-16 14:22:17 -0300

    Instalar swagger ui / criar arquivo yaml da api / criar index para abrir swagger no github

--- PassIn/PassIn.Api/Program.cs
+++ PassIn/PassIn.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using PassIn.Api.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
@@ -16,7 +20,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c=> {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
